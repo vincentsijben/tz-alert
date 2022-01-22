bad53b4319b7666d2adede9b5881bd0822a185b0
Vincent Sijben <vincent.sijben@zuyd.nl>
2022-01-22 15:20:06 +0100

    fixed script

--- install-server.sh
+++ install-server.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
-# for digitalocean droplet using Ubuntu 20.04 (LTS) x64
+# Tested with DigitalOcean droplet using Ubuntu 20.04 (LTS) x64
 
-# install node, it will run run apt-get update automatically:
+# - Create a new droplet in the Digital Ocean interface
+# - set up SSH keys
+# - connect to your droplet
+
+# install node (quietly), it will run run apt-get update automatically:
 curl -sL https://deb.nodesource.com/setup_current.x | sudo -E bash -
 sudo apt -qq install -y nodejs
 # install pm2 globally
 sudo npm install -g pm2
-# sudo apt install -y git-all
 sudo apt install -y git
 git clone https://github.com/vincentsijben/tz-alert.git /opt/tz-alert/
 cd /opt/tz-alert/
@@ -15,5 +18,9 @@ npm i
 # create the config file
 mv config/config.json.example config/config.json
 echo
+echo "=========================================="
 echo "Now put your own token in the config.json!"
+echo
+echo "You can use: nano config/config.json"
+echo "=========================================="
 echo
